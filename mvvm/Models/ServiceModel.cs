5af0882200313e52f07250b9d5310651fd2b9080
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-11-23 23:18:28 +0000

    init

---
+++ mvvm/Models/ServiceModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MVVM
+{
+    class ServiceModel
+    {
+        public event EventHandler<FlickCamerasEventArgs> GetCamerasCompleted;
+        // método asincrono que devuelve lista de cámaras
+        public void getCameras()
+        {
+            string uri = "https://api.flickr.com/services/rest/?method=flickr.cameras.getBrands&api_key=c9ea2337723bf09a027e0258c59c505f&format=rest";
+
+            // el web client permite comunicarse de forma http con algún endpoint
+            WebClient client = new WebClient();
+
+            client.DownloadStringCompleted += (s, a) =>
+                {
+                    if (a.Error == null && !a.Cancelled)
+                    {
+                        var result = a.Result;
+
+                        // LINQ para XML
+
+                        // xdocument permite interpretar una cadena
+                        var doc = XDocument.Parse(result);
+
+                        // en doc ya está el documento en xml
+
+                        // realizamos una consulta para cada uno de los elementos que sea descendiente del elemento brand
+                        var query = from c in doc.Descendants("brand")
+                                    select new Camera() { Name = c.Attribute("name").Value }; // creamos una nueva cámara y la instanciamos con el nombre de la cámara que está en el archivo de xml
+
+                        // al ejecutar el método ToList regresa una lista de tipo cámara en este caso es así porque se instanciaron elemento de tipo cámara
+                        var results = query.ToList();
+
+                        if (GetCamerasCompleted != null)
+                        {
+                            GetCamerasCompleted(this, new FlickCamerasEventArgs(results));
+                        }
+                    }
+                };
+            client.DownloadStringAsync(new Uri(uri,UriKind.Absolute));
+        }
+    }
+}
---
+++ mvvm/ViewModels/CamerasViewModel.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.ViewModels
+{
+    public class CamerasViewModel : NotificationEnabledObject
+    {
+        // el ObservableCollection es adecuado para enlaces de datos en las tecnologías xaml
+        private ObservableCollection <Camera> cameraList;
+        ServiceModel serviceModel = new ServiceModel();
+
+        public CamerasViewModel()
+        {
+            DeserializeXML();
+            serviceModel.GetCamerasCompleted += (s, a) =>
+                {
+                    cameraList = new ObservableCollection<Camera>(a.Results);
+
+                    // llamamos el método para que serialice cada elemento de la lista
+                    SerializeXML();
+
+                };
+        }
+
+        const string fileName = "Datos.xml";
+        void SerializeXML()
+        {
+            // serializar los datos en XML, en tipo ObservableCollection<Camera>
+            DataContractSerializer serializer = new DataContractSerializer(typeof(ObservableCollection<Camera>));
+
+            using (var isf = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                // verificamos si el archivo existe
+                if (isf.FileExists(fileName))
+                {
+                    isf.DeleteFile(fileName); // si es así entonces lo eliminamos
+                }
+
+                 using (var stream = isf.CreateFile(fileName))
+                 {
+                     // escribimos en ese stream los resultados
+                     serializer.WriteObject(stream, cameraList);
+                     stream.Close(); // cerramos el stream
+                 }
+            }
+
+           
+        }
+
+        void DeserializeXML()
+        {
+             // verificamos si no se está en tiempo de diseño
+            if (!DesignerProperties.IsInDesignTool)
+            {
+                // obtenemos el almacenamiento a
+                using (var isf = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    // verificamos si el archivo existe
+                    if (isf.FileExists(fileName))
+                    {
+                        // si es así entonces lo vamos a leer
+
+                        using (var stream = isf.OpenFile(fileName,System.IO.FileMode.Open))
+                        {
+                            // serializar los datos en XML, en tipo ObservableCollection<Camera>
+                            DataContractSerializer serializer = new DataContractSerializer(typeof(ObservableCollection<Camera>));
+
+                            // leemos los datos y los deserializamos
+                            var data = serializer.ReadObject(stream) as ObservableCollection<Camera>;
+
+                            // verificamos si data tiene algo
+                            if (data != null && data.Count > 0)
+                            {
+                                // si tiene datos entoces
+                                cameraList = data; // establecemos la lista igual a lo que acbamos de leer
+                            }
+                            stream.Close(); // cerramos el stream
+                        }
+                    }
+
+                   
+                }
+            }
+
+        }
+        public ObservableCollection <Camera> CameraList
+        {
+            get 
+            { 
+                if (cameraList == null)
+                {
+                    cameraList = new ObservableCollection<Camera>();
+                }
+
+                // esto es para agregar elementos y tener blendability, si se está en tiempo de diseño entonces se muestren datos de ejemplo
+                if(DesignerProperties.IsInDesignTool)
+                {
+                    for (int i = 0 ; i < 20 ; i++)
+                    {
+                        cameraList.Add(new Camera() { Name = "Camara #" });
+                    }
+                }
+
+                return cameraList; 
+            }
+            set 
+            { 
+                cameraList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        ActionCommand getCamerasCommand;
+        public ActionCommand GetCamerasCommand
+        {
+            get 
+            { 
+                if (getCamerasCommand == null)
+                {
+                    getCamerasCommand = new ActionCommand(() => 
+                    {
+                        serviceModel.getCameras();
+                    });
+                }
+                return getCamerasCommand;
+            }
+            
+        }
+        
+    }
+}
